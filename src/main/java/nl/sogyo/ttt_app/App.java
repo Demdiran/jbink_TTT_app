a5e8cd8a24a1efb87fd1f8ebbb9099823e6bd675
jcbink <jbink@sogyo.nl>
2020-03-09 10:21:44 +0100

    setup of standard maven project

---
+++ src/main/java/nl/sogyo/ttt_app/App.java
@@ -0,0 +1,13 @@
+package nl.sogyo.ttt_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
