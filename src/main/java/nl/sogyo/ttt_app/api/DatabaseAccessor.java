cdfc0e774f9cd5e61a4e6dba71689d2a386a5bdf
jcbink <jbink@sogyo.nl>
2020-03-11 15:10:24 +0100

    Repeatable tests for DatabaseAccessor

--- src/main/java/nl/sogyo/ttt_app/api/DatabaseAccessor.java
+++ src/main/java/nl/sogyo/ttt_app/api/DatabaseAccessor.java
@@ -10,13 +10,12 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import nl.sogyo.ttt_app.domain.*;
 
 class DatabaseAccessor{
-	static SessionFactory sessionFactory = buildSessionFactory();
 	private Session hibernateSession;
-	private static SessionFactory buildSessionFactory() {
+	public static SessionFactory buildSessionFactory(String settings) {
 		try {
 			// Create the SessionFactory from hibernate.cfg.xml
 			StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
-					.configure("hibernate.cfg.xml").build();
+					.configure(settings).build();
 			Metadata metadata = new MetadataSources(standardRegistry).getMetadataBuilder().build();
 			return metadata.getSessionFactoryBuilder().build();
 		} catch (Throwable ex) {
@@ -28,8 +27,18 @@ class DatabaseAccessor{
 
 	public DatabaseAccessor(){
 		java.util.logging.Logger.getLogger("org.hibernate").setLevel(java.util.logging.Level.OFF);
-		hibernateSession = sessionFactory.openSession();
+		hibernateSession = buildSessionFactory("hibernate.cfg.xml").openSession();
 		java.util.logging.Logger.getLogger("org.hibernate").setLevel(java.util.logging.Level.OFF);
+	}
+
+	public DatabaseAccessor(Session hibernateSession){
+		this.hibernateSession = hibernateSession;
+	}
+
+	public void dispose(){
+		if(hibernateSession.isOpen()){
+			hibernateSession.close();
+		}
 	}
 	
 	public Player getPlayer(int ID){
@@ -44,11 +53,6 @@ class DatabaseAccessor{
 				hibernateSession.getTransaction().rollback();
 			}
 			sqlException.printStackTrace();
-		} finally {
-			if (hibernateSession != null && hibernateSession.isOpen()) {
-				hibernateSession.close();
-			}
-			System.out.println("\n.......Hibernate session closed!.......");
 		}
 		return player;
 	}
@@ -98,12 +102,7 @@ class DatabaseAccessor{
 	}
 
 	public void mergeTournament(Tournament tournament){
-		Session hibernateSession = null;
 		try {
-			java.util.logging.Logger.getLogger("org.hibernate").setLevel(java.util.logging.Level.OFF);
-			hibernateSession = sessionFactory.openSession();
-			java.util.logging.Logger.getLogger("org.hibernate").setLevel(java.util.logging.Level.OFF);
-
 			hibernateSession.beginTransaction();
 			hibernateSession.merge(tournament);
 			hibernateSession.getTransaction().commit();
--- src/test/java/nl/sogyo/ttt_app/api/TestDatabaseAccessor.java
+++ src/test/java/nl/sogyo/ttt_app/api/TestDatabaseAccessor.java
@@ -2,44 +2,76 @@ package nl.sogyo.ttt_app.api;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.hibernate.Session;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import nl.sogyo.ttt_app.domain.*;
 public class TestDatabaseAccessor{
+    Session hibernateSession;
+    DatabaseAccessor databaseAccessor;
 
-    // @Test
-    // public void TestGetPlayerByID(){
-    //     DatabaseAccessor databaseAccessor = new DatabaseAccessor();
-    //     Player testplayerin = new Player();
-    //     testplayerin.setRating(800);
-    //     testplayerin.setName("testplayer1");
-    //     testplayerin.setID(1);
-    //     databaseAccessor.mergePlayer(testplayerin);
-    //     Player testplayer = databaseAccessor.getPlayer(1);
-    //     assertEquals("testplayer1", testplayer.getName());
-    //     assertEquals(800, testplayer.getRating());
-    //     databaseAccessor.removePlayer(1);
-    // }
+    @BeforeEach 
+    public void databaseAccessorSetup(){
+        hibernateSession = DatabaseAccessor.buildSessionFactory("hibernateTest.cfg.xml").openSession();
+        databaseAccessor = new DatabaseAccessor(hibernateSession);
+    }
 
-    // @Test
-    // public void TestManyToMany(){
-    //     DatabaseAccessor databaseAccessor = new DatabaseAccessor();
-    //     Player testplayer1 = new Player();
-    //     testplayer1.setRating(800);
-    //     testplayer1.setName("testplayer1");
-    //     Player testplayer2 = new Player();
-    //     testplayer2.setRating(1000);
-    //     testplayer2.setName("testplayer2");
-    //     Tournament testTournament1 = new Tournament();
-    //     testTournament1.setName("testtournament1");
-    //     Tournament testTournament2 = new Tournament();
-    //     testTournament2.setName("testtournament2");
+    @AfterEach
+    public void databaseAccessorTeardown(){
+        if(hibernateSession.isOpen())
+            hibernateSession.close();
+    }
 
-    //     testplayer1.signUpForTournament(testTournament1);
-    //     testplayer1.signUpForTournament(testTournament2);
-    //     testplayer2.signUpForTournament(testTournament1);
+    @Test
+    public void TestGetPlayerByID(){
+        Player testplayerin = new Player();
+        testplayerin.setRating(800);
+        testplayerin.setName("testplayer1");
+        databaseAccessor.persistPlayer(testplayerin);
 
-    //     databaseAccessor.persistPlayer(testplayer1);
-    //     databaseAccessor.persistPlayer(testplayer2);
-    // }
+        hibernateSession.clear();
+
+        Player testplayer = databaseAccessor.getPlayer(testplayerin.getID());
+        assertEquals("testplayer1", testplayer.getName());
+        assertEquals(800, testplayer.getRating());
+    }
+
+    @Test
+    public void TestManyToMany(){
+        Player testplayer1 = new Player();
+        testplayer1.setRating(800);
+        testplayer1.setName("testplayer1");
+        Player testplayer2 = new Player();
+        testplayer2.setRating(1000);
+        testplayer2.setName("testplayer2");
+        Tournament testTournament1 = new Tournament();
+        testTournament1.setName("testtournament1");
+        Tournament testTournament2 = new Tournament();
+        testTournament2.setName("testtournament2");
+
+        testplayer1.signUpForTournament(testTournament1);
+        testplayer1.signUpForTournament(testTournament2);
+        testplayer2.signUpForTournament(testTournament1);
+
+        databaseAccessor.persistPlayer(testplayer1);
+        databaseAccessor.persistPlayer(testplayer2);
+
+        hibernateSession.clear();
+
+        Player testplayer1out = databaseAccessor.getPlayer(testplayer1.getID());
+        Player testplayer2out = databaseAccessor.getPlayer(testplayer2.getID());
+        Tournament testtTournament1out = databaseAccessor.getTournament(testTournament1.getTournamentID());
+        Tournament testtTournament2out = databaseAccessor.getTournament(testTournament2.getTournamentID());
+
+        assertEquals("testplayer1", testplayer1out.getName());
+        assertEquals(800, testplayer1out.getRating());
+        assertEquals("testplayer2", testplayer2out.getName());
+        assertEquals(1000, testplayer2out.getRating());
+        assert(testplayer1out.getTournaments().contains(testtTournament1out));
+        assert(testplayer1out.getTournaments().contains(testtTournament2out));
+        assert(testtTournament1out.getParticipants().contains(testplayer2out));
+    
+    }
 }
